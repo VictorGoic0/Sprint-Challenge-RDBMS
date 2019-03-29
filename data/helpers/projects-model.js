e9e07a78e9fad59bce1307c6e854e7be6594fc0a
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-29 13:21:19 -0400

    All objects returned by a response will now have true or false in completed rather than 0 or 1

--- data/helpers/projects-model.js
+++ data/helpers/projects-model.js
@@ -10,6 +10,11 @@ module.exports = {
 
 async function find() {
   const projects = await db("projects");
+  projects.forEach(project =>
+    project.completed === 1
+      ? (project.completed = true)
+      : (project.completed = false)
+  );
   return projects;
 }
 
@@ -34,6 +39,14 @@ async function findById(id) {
     .where({
       "actions.project_id": id
     });
+  project.completed === 1
+    ? (project.completed = true)
+    : (project.completed = false);
+  project.actions.forEach(action =>
+    action.completed === 1
+      ? (action.completed = true)
+      : (action.completed = false)
+  );
 
   return project;
 }
@@ -42,6 +55,9 @@ async function create(item) {
   const [id] = await db("projects").insert(item);
   if (id) {
     const project = await findById(id);
+    project.completed === 1
+      ? (project.completed = true)
+      : (project.completed = false);
     return project;
   }
 }
